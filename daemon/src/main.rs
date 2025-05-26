f7664b31b2363bddb70b1656ad3ab414eddd9074
zefr0x <zer0-x.7ty50@aleeas.com>
2025-05-26 21:36:46 +0300

    build(cargo): initiate rust project with initial structure
    Initiate `core` and `daemon` workspaces with plans to have few more as we
    progress in development (e.g. database, networking, informants).

---
+++ daemon/src/main.rs
@@ -0,0 +1,7 @@
+//! The official daemon and API implementation for Aleym.
+//!
+//! This daemon represents Aleym with an API for communication with user interfaces (e.g. feed readers).
+
+fn main() {
+	unimplemented!()
+}
